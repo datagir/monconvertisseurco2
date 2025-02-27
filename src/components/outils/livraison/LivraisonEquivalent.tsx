datagir/impactco2

feat: new livraison simulator

src/components/outils/livraison/LivraisonEquivalent.tsx
@@ -1,0 +1,56 @@
+'use client'
+
+import classNames from 'classnames'
+import { useTranslations } from 'next-intl'
+import useParamContext from 'src/providers/ParamProvider'
+import { ComputedEquivalent } from 'types/equivalent'
+import { getNameWithoutSuffix } from 'utils/Equivalent/equivalent'
+import formatNumber from 'utils/formatNumber'
+import EquivalentIcon from 'components/base/EquivalentIcon'
+import LocalNumber from 'components/base/LocalNumber'
+import styles from './LivraisonEquivalent.module.css'
+
+const LivraisonEquivalent = ({
+  animated,
+  equivalent,
+  max,
+  index,
+}: {
+  animated?: boolean
+  equivalent: ComputedEquivalent
+  max: number
+  index: number
+}) => {
+  const { language, livraison } = useParamContext()
+  const t = useTranslations('livraison')
+
+  return (
+    <div
+      className={classNames(styles.container, {
+        [styles.static]: !animated,
+      })}>
+      <EquivalentIcon equivalent={equivalent} height={2.5} />
+      <div className={styles.info}>
+        <p>
+          {getNameWithoutSuffix(language, equivalent)}
+          {livraison.distance[equivalent.slug] && <span className={styles.gray}> - {t('car')}</span>}
+          {equivalent.slug.includes('douce') && <span className={styles.green}> - {t('car')}</span>}
+        </p>
+        <div className={styles.barContainer}>
+          <div
+            className={classNames(styles.bar, {
+              [styles.greenBar]: index < 2,
+              [styles.plainBar]: index > 4,
+            })}
+            style={{ width: `${(70 * equivalent.value) / max}%` }}
+          />
+          <p className={styles.value}>
+            <LocalNumber number={formatNumber(equivalent.value)} /> kg CO₂e
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LivraisonEquivalent

teste/livraison.spec.ts
@@ -1,44 +1,75 @@
 import { expect, test } from '@playwright/test'
 
-test('Simulator livraison', async ({ page }) => {
+test('Livraison simulator', async ({ page }) => {
   await page.goto('http://localhost:3000/outils/livraison')
 
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('2.46')
-  await expect(page.getByTestId('livraison-habits-value')).toHaveText('29.6')
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('6.15')
+  await page.getByTestId('text-select-type').click()
+  await page.getByTestId('text-select-type').selectOption('lavelinge')
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('25.4')
+  await page.getByTestId('checkbox-fabrication').click()
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('366')
+  await page.getByTestId('checkbox-fabrication').click({ force: true })
 
-  await expect(page.getByTestId('etiquette-value')).toHaveText('2.46')
+  await expect(page.getByTestId('category-pointrelais-value')).toHaveText('28.5')
+  await expect(page.getByTestId('livraison-pointrelais')).toHaveText(
+    "Transport utilisé pour aller chercher l'objetVoiture thermiqueVoiture électrique7 km Distance parcourue pour Livraison en point relais à 7km"
+  )
+  await page.getByRole('button', { name: 'Augmenter le nombre de km' }).first().click()
+  await expect(page.getByTestId('livraison-pointrelais')).toHaveText(
+    "Transport utilisé pour aller chercher l'objetVoiture thermiqueVoiture électrique8 km Distance parcourue pour Livraison en point relais à 7km"
+  )
+  await expect(page.getByTestId('category-pointrelais-value')).toHaveText('28.7')
+  await page.getByTestId('text-select-transport-type-pointrelais').click()
+  await page.getByTestId('text-select-transport-type-pointrelais').selectOption('voitureelectrique')
+  await expect(page.getByTestId('category-pointrelais-value')).toHaveText('27.8')
 
-  await page.getByTestId('text-select-livraison-produit').selectOption('grande consommation')
-  await expect(page.getByText("Le point relais est t'il sur")).toBeVisible()
-  await expect(page.getByTestId('input-km-value')).toBeVisible()
-  await expect(page.getByTestId('text-select-km-type')).toBeVisible()
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('10.2')
+  await page.getByTestId('header-share-button').first().click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    'http://localhost:3000/outils/livraison?withFabrication=false&language=fr'
+  )
+  await page.getByTestId('custom-param-withFabrication-checkbox').click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    'http://localhost:3000/outils/livraison?withFabrication=true&language=fr'
+  )
+  await page.getByRole('button', { name: 'Fermer' }).first().click()
 
-  await page.getByLabel('Oui', { exact: true }).check()
-  await expect(page.getByTestId('input-km-value')).not.toBeVisible()
-  await expect(page.getByTestId('text-select-km-type')).not.toBeVisible()
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('8.73')
+  await page.getByTestId('header-integrate-button').first().click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    '<script name="impact-co2" src="http://localhost:3000/iframe.js" data-type="/livraison" data-search="?&withFabrication=true&theme=default&language=fr"></script>'
+  )
+  await page.getByTestId('custom-param-withFabrication-checkbox').click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    '<script name="impact-co2" src="http://localhost:3000/iframe.js" data-type="/livraison" data-search="?&theme=default&language=fr"></script>'
+  )
+  await page.getByLabel('Intégrer').getByText('Courses alimentaires').click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    '<script name="impact-co2" src="http://localhost:3000/iframe.js" data-type="/livraison" data-search="?&theme=default&language=fr&types=chaussure,livre,microondes,vetements,lavelinge,lit,smartphone,vin,cafetiere"></script>'
+  )
+  await page.getByLabel('Intégrer').getByText('Paire de chaussures').click()
+  await page.getByLabel('Intégrer').getByText('Micro-ondes').click()
+  await page.getByLabel('Intégrer').getByText('Livres').click()
+  await page.getByLabel('Intégrer').getByText('Commande de vêtements').click()
+  await page.getByLabel('Intégrer').getByText('Lit complet').click()
+  await page.getByLabel('Intégrer').getByText('Caisse de vin').click()
+  await page.getByLabel('Intégrer').getByText('Smartphone', { exact: true }).click()
+  await page.getByLabel('Intégrer').getByText('Lave-linge').click()
+  await expect(page.getByLabel('Intégrer').getByText('Cafetière espresso')).toBeDisabled()
+})
 
-  await await page.getByRole('group', { name: "Le point relais est t'il sur" }).locator('label').first().click()
-  await page.getByTestId('input-km-value').click()
-  await page.getByTestId('input-km-value').fill('10')
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('10.9')
+test('Livraison iframes', async ({ page }) => {
+  await page.goto(
+    'http://localhost:3000/iframes/livraison?withFabrication=true&types=lavelinge,lit,smartphone,vin,cafetiere'
+  )
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('366')
 
-  await page.getByTestId('text-select-km-type').selectOption('vélo')
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('8.73')
-
-  await page.getByTestId('text-select-livraison-retrait').selectOption('domicile')
-  await expect(page.getByText("Le point relais est t'il sur")).not.toBeVisible()
-  await expect(page.getByTestId('input-km-value')).not.toBeVisible()
-  await expect(page.getByTestId('text-select-km-type')).not.toBeVisible()
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('8.73')
-
-  await page.getByLabel('Oui (transport par avion)').check()
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('225')
-
-  await expect(page.getByTestId('livraison-habits-value')).toHaveText('2,698')
-  await page.getByTestId('input-number-value').fill('3')
-  await expect(page.getByTestId('livraison-habits-value')).toHaveText('8,095')
-  await page.getByTestId('text-select-frequence-type').selectOption('52')
-  await expect(page.getByTestId('livraison-habits-value')).toHaveText('35,080')
+  const select = await page.getByTestId('text-select-type')
+  const options = await select.evaluate((select) =>
+    Array.from((select as HTMLSelectElement).options).map((option) => option.value)
+  )
+  expect(options).not.toContain('voitureelectrique')
+  await page.goto(
+    'http://localhost:3000/iframes/livraison?withFabrication=false&types=lavelinge,lit,smartphone,vin,cafetiere'
+  )
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('25.4')
 })

datagir/monconvertisseurco2

src/components/outils/livraison/LivraisonEquivalent.tsx
@@ -1,0 +1,58 @@
+'use client'
+
+import classNames from 'classnames'
+import { useTranslations } from 'next-intl'
+import useParamContext from 'src/providers/ParamProvider'
+import { ComputedEquivalent } from 'types/equivalent'
+import { getNameWithoutSuffix } from 'utils/Equivalent/equivalent'
+import formatNumber from 'utils/formatNumber'
+import EquivalentIcon from 'components/base/EquivalentIcon'
+import IframeableLink from 'components/base/IframeableLink'
+import LocalNumber from 'components/base/LocalNumber'
+import styles from './LivraisonEquivalent.module.css'
+
+const LivraisonEquivalent = ({
+  animated,
+  equivalent,
+  max,
+  index,
+}: {
+  animated?: boolean
+  equivalent: ComputedEquivalent
+  max: number
+  index: number
+}) => {
+  const { language, livraison } = useParamContext()
+  const t = useTranslations('livraison')
+
+  return (
+    <IframeableLink
+      className={classNames(styles.container, {
+        [styles.static]: !animated,
+      })}
+      href={`/outils/livraison/${equivalent.slug}`}>
+      <EquivalentIcon equivalent={equivalent} height={2.5} />
+      <div className={styles.info}>
+        <p>
+          {getNameWithoutSuffix(language, equivalent)}
+          {livraison.distance[equivalent.slug] && <span className={styles.gray}> - {t('car')}</span>}
+          {equivalent.slug.includes('douce') && <span className={styles.green}> - {t('foot')}</span>}
+        </p>
+        <div className={styles.barContainer}>
+          <div
+            className={classNames(styles.bar, {
+              [styles.greenBar]: index < 2,
+              [styles.plainBar]: index > 4,
+            })}
+            style={{ width: `${(70 * equivalent.value) / max}%` }}
+          />
+          <p className={styles.value}>
+            <LocalNumber number={formatNumber(equivalent.value)} /> kg CO₂e
+          </p>
+        </div>
+      </div>
+    </IframeableLink>
+  )
+}
+
+export default LivraisonEquivalent

teste/livraison.spec.ts
@@ -1,44 +1,75 @@
 import { expect, test } from '@playwright/test'
 
-test('Simulator livraison', async ({ page }) => {
+test('Livraison simulator', async ({ page }) => {
   await page.goto('http://localhost:3000/outils/livraison')
 
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('2.46')
-  await expect(page.getByTestId('livraison-habits-value')).toHaveText('29.6')
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('6.15')
+  await page.getByTestId('text-select-type').click()
+  await page.getByTestId('text-select-type').selectOption('lavelinge')
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('25.4')
+  await page.getByTestId('checkbox-fabrication').click()
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('300')
+  await page.getByTestId('checkbox-fabrication').click({ force: true })
 
-  await expect(page.getByTestId('etiquette-value')).toHaveText('2.46')
+  await expect(page.getByTestId('category-pointrelais-value')).toHaveText('28.3')
+  await expect(page.getByTestId('livraison-pointrelais')).toHaveText(
+    "Transport utilisé pour aller chercher l'objetVoiture thermiqueVoiture électrique3 km Distance parcourue pour Livraison en point relais à 7km"
+  )
+  await page.getByRole('button', { name: 'Augmenter le nombre de km' }).first().click()
+  await expect(page.getByTestId('livraison-pointrelais')).toHaveText(
+    "Transport utilisé pour aller chercher l'objetVoiture thermiqueVoiture électrique4 km Distance parcourue pour Livraison en point relais à 7km"
+  )
+  await expect(page.getByTestId('category-pointrelais-value')).toHaveText('28.7')
+  await page.getByTestId('text-select-transport-type-pointrelais').click()
+  await page.getByTestId('text-select-transport-type-pointrelais').selectOption('voitureelectrique')
+  await expect(page.getByTestId('category-pointrelais-value')).toHaveText('27.8')
 
-  await page.getByTestId('text-select-livraison-produit').selectOption('grande consommation')
-  await expect(page.getByText("Le point relais est t'il sur")).toBeVisible()
-  await expect(page.getByTestId('input-km-value')).toBeVisible()
-  await expect(page.getByTestId('text-select-km-type')).toBeVisible()
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('10.2')
+  await page.getByTestId('header-share-button').first().click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    'http://localhost:3000/outils/livraison?withFabrication=false&language=fr'
+  )
+  await page.getByTestId('custom-param-withFabrication-checkbox').click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    'http://localhost:3000/outils/livraison?withFabrication=true&language=fr'
+  )
+  await page.getByRole('button', { name: 'Fermer' }).first().click()
 
-  await page.getByLabel('Oui', { exact: true }).check()
-  await expect(page.getByTestId('input-km-value')).not.toBeVisible()
-  await expect(page.getByTestId('text-select-km-type')).not.toBeVisible()
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('8.73')
+  await page.getByTestId('header-integrate-button').first().click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    '<script name="impact-co2" src="http://localhost:3000/iframe.js" data-type="/livraison" data-search="?&withFabrication=true&theme=default&language=fr"></script>'
+  )
+  await page.getByTestId('custom-param-withFabrication-checkbox').click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    '<script name="impact-co2" src="http://localhost:3000/iframe.js" data-type="/livraison" data-search="?&theme=default&language=fr"></script>'
+  )
+  await page.getByLabel('Intégrer').getByText('Courses alimentaires').click()
+  await expect(page.getByTestId('clipboard-box')).toHaveText(
+    '<script name="impact-co2" src="http://localhost:3000/iframe.js" data-type="/livraison" data-search="?&theme=default&language=fr&types=chaussure,livre,microondes,vetements,lavelinge,lit,smartphone,vin,cafetiere"></script>'
+  )
+  await page.getByLabel('Intégrer').getByText('Paire de chaussures').click()
+  await page.getByLabel('Intégrer').getByText('Micro-ondes').click()
+  await page.getByLabel('Intégrer').getByText('Livres').click()
+  await page.getByLabel('Intégrer').getByText('Commande de vêtements').click()
+  await page.getByLabel('Intégrer').getByText('Lit complet').click()
+  await page.getByLabel('Intégrer').getByText('Caisse de vin').click()
+  await page.getByLabel('Intégrer').getByText('Smartphone', { exact: true }).click()
+  await page.getByLabel('Intégrer').getByText('Lave-linge').click()
+  await expect(page.getByLabel('Intégrer').getByText('Cafetière espresso')).toBeDisabled()
+})
 
-  await await page.getByRole('group', { name: "Le point relais est t'il sur" }).locator('label').first().click()
-  await page.getByTestId('input-km-value').click()
-  await page.getByTestId('input-km-value').fill('10')
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('10.9')
+test('Livraison iframes', async ({ page }) => {
+  await page.goto(
+    'http://localhost:3000/iframes/livraison?withFabrication=true&types=lavelinge,lit,smartphone,vin,cafetiere'
+  )
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('300')
 
-  await page.getByTestId('text-select-km-type').selectOption('vélo')
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('8.73')
-
-  await page.getByTestId('text-select-livraison-retrait').selectOption('domicile')
-  await expect(page.getByText("Le point relais est t'il sur")).not.toBeVisible()
-  await expect(page.getByTestId('input-km-value')).not.toBeVisible()
-  await expect(page.getByTestId('text-select-km-type')).not.toBeVisible()
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('8.73')
-
-  await page.getByLabel('Oui (transport par avion)').check()
-  await expect(page.getByTestId('livraison-colis-value')).toHaveText('225')
-
-  await expect(page.getByTestId('livraison-habits-value')).toHaveText('2,698')
-  await page.getByTestId('input-number-value').fill('3')
-  await expect(page.getByTestId('livraison-habits-value')).toHaveText('8,095')
-  await page.getByTestId('text-select-frequence-type').selectOption('52')
-  await expect(page.getByTestId('livraison-habits-value')).toHaveText('35,080')
+  const select = await page.getByTestId('text-select-type')
+  const options = await select.evaluate((select) =>
+    Array.from((select as HTMLSelectElement).options).map((option) => option.value)
+  )
+  expect(options).not.toContain('voitureelectrique')
+  await page.goto(
+    'http://localhost:3000/iframes/livraison?withFabrication=false&types=lavelinge,lit,smartphone,vin,cafetiere'
+  )
+  await expect(page.getByTestId('category-magasindouce-value')).toHaveText('25.4')
 })